127fa27ba4e51ef2b77f15582ce06a57d70bbc66
ThinkSpid <rbb428@gmail.com>
2019-10-21 18:41:14 -0400

    I saw your code. I first try to run the G[i] first. I change some code to make the image output more easily. See the output as p1 to p4 By the way it consume a lot time to process the figure. I will try to implement other two you mentioned. Also i want to try to make a copy on Colab cause i thought i may be more quicker. But thanks for your research. I think it is a good start!

--- Superpixel_overseg/super_pix_diff_normalization.py
+++ Superpixel_overseg/super_pix_diff_normalization.py
@@ -21,17 +21,19 @@ def norm_array(X, x_min = 0, x_max = 1):
 #Read the input image
 X_f = dip.imread('tiger.jpg')
 
+dip.figure(1)
 # display the input image
 dip.imshow(X_f)
-dip.show()
+plt.savefig('p1.png',dpi=300,bbox_inches='tight',pad_inches=0.1)
 
 # Calculate the superpixels. The output of slic is an image with the pixel values as superpixel indices. The size
 # of the image is same as the input image.
 super_pixels = slic(X_f, n_segments = 500, sigma = 5)
 
 # This is to visualize the boundaries of superpixels
+dip.figure(2)
 dip.imshow(mark_boundaries(X_f, super_pixels))
-dip.show()
+plt.savefig('p2.png',dpi=300,bbox_inches='tight',pad_inches=0.1)
 
 # Get the number of superpixels
 num_super_pix = len(np.unique(super_pixels))
@@ -101,9 +103,9 @@ for n in range(num_super_pix):
         for j in range(size_N):
             if super_pixels[i][j] == n:
                 G_s_img[i][j] = G_s[n]
-
+dip.figure(3)
 dip.imshow(G_s_img, 'gray')
-dip.show()
+plt.savefig('p3.png',dpi=300,bbox_inches='tight',pad_inches=0.1)
 
 # Get the binary version of G_s according to the threshold thresh_G
 G_s_bin = np.zeros((num_super_pix, 1))
@@ -124,10 +126,10 @@ for n in range(num_super_pix):
         for j in range(size_N):
             if super_pixels[i][j] == n:
                 img_G_s_bin[i][j] = G_s_bin[n]
-
+dip.figure(4)
 dip.imshow(img_G_s_bin, 'gray')
-dip.show()
-
+# dip.show()
+plt.savefig('p4.png',dpi=300,bbox_inches='tight',pad_inches=0.1)
 # We need to follow a similar process to find I_s[i] and O_s[i]. All we need to do is replace the mu_super_pix with a
 # different feature, such as intensity in case of I_s[i]. For U_s[i], we need to just penalize the distance of from the
 # superpixel from centre of the image, so something like e^{-w[P'_s[i] - P_c]^2} should work.
